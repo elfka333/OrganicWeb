4e7819d04b47c602082a69ef89f2234f2428e495
elfka333 <elfka333@mail.ru>
2021-06-30 14:12:38 +0300

    add

--- src/js/script.js
+++ src/js/script.js
@@ -13,5 +13,19 @@ $(document).ready(function () {
       $input.change();
       return false;
     });
+    $(".catalog-count__minus").click(function () {
+      var $input = $(this).parent().find("input");
+      var count = parseInt($input.val()) - 1;
+      count = count < 1 ? 1 : count;
+      $input.val(count);
+      $input.change();
+      return false;
+    });
+    $(".catalog-count__plus").click(function () {
+      var $input = $(this).parent().find("input");
+      $input.val(parseInt($input.val()) + 1);
+      $input.change();
+      return false;
+    });
   });
   
